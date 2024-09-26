a8dadadbe4c421c98b5938683b045682e1604707
Mihai <mcosma@gmail.com>
2024-09-26 18:19:02 -0400

    initial commit

---
+++ web3_utils.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import time
+import traceback
+
+from dotenv import load_dotenv
+from web3 import Web3
+
+# pylint: disable=redefined-builtin
+
+load_dotenv()
+ETH_NODE_URL = os.getenv("ETH_NODE_URL")
+# defaults to http://localhost:8545 if you don't set ETH_NODE_URL
+w3 = Web3(Web3.HTTPProvider(ETH_NODE_URL))
+
+def fetch_events_logs_with_retry(
+    label: str,
+    contract_event,
+    from_block: int,
+    to_block: int | str = "latest",
+    retries: int = 3,
+    delay: int = 2,
+    filter: dict | None = None,
+) -> dict | None:
+    for attempt in range(retries):
+        try:
+            if filter is None:
+                return contract_event.get_logs(fromBlock=from_block, toBlock=to_block)
+            else:
+                return contract_event.get_logs(filter)
+        except Exception as e:
+            if attempt < retries - 1:
+                time.sleep(delay)
+                continue
+            else:
+                msg = f"Error getting events logs for {label}: {e}, {traceback.format_exc()}"
+                logging.error(msg)
+                raise e
