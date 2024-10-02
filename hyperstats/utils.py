20547319efccfbeae730ad445066d6eda5cdfc90
Mihai <mcosma@gmail.com>
2024-10-02 11:55:07 -0400

    proper packaging 5

--- hyperstats/utils.py
+++ hyperstats/utils.py
@@ -7,7 +7,7 @@ import eth_abi
 from dotenv import load_dotenv
 from tqdm import tqdm
 
-from hyperstats.constants import (
+from .constants import (
     ERC20_ABI,
     HYPERDRIVE_MORPHO_ABI,
     HYPERDRIVE_REGISTRY_ABI,
@@ -15,7 +15,7 @@ from hyperstats.constants import (
     MORPHO_ABI,
     HyperdrivePrefix,
 )
-from hyperstats.web3_utils import (
+from .web3_utils import (
     fetch_events_logs_with_retry,
     w3,
 )
